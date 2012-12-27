d7e5feb487d96b0f85012ce5289952336679325c
Steve Yen <steve.yen@gmail.com>
2012-12-26 19:32:50 -0800

    Added NewTreap().

--- treap.go
+++ treap.go
@@ -1,14 +1,17 @@
 package treap
 
 type Treap struct {
-	compare Compare
-	root    node
+	compare    Compare
+	prioritize Prioritize
+	root       *node
 }
 
 // Compare returns an integer comparing the two items
 // lexicographically. The result will be 0 if a==b, -1 if a < b, and
 // +1 if a > b.
 type Compare func(a, b Item) int
+
+type Prioritize func(a Item) int
 
 // Item can be anything.
 type Item interface{}
@@ -18,4 +21,8 @@ type node struct {
 	priority int
 	left     *node
 	right    *node
+}
+
+func NewTreap(c Compare, p Prioritize) *Treap {
+	return &Treap{compare: c, prioritize: p, root: nil}
 }
--- treap_test.go
+++ treap_test.go
@@ -5,4 +5,8 @@ import (
 )
 
 func TestTreap(t *testing.T) {
+	x := NewTreap(nil, nil)
+	if x == nil {
+		t.Errorf("expected NewTreap to work")
+	}
 }
