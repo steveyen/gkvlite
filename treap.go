9d2c73e3afcc9ea76eaa0b9e33aba4edb23bd936
Steve Yen <steve.yen@gmail.com>
2012-12-26 22:15:40 -0800

    Iterative node get().

--- treap.go
+++ treap.go
@@ -1,9 +1,9 @@
 package treap
 
 type Treap struct {
-	compare    Compare
-	count      int
-	root       *node
+	compare Compare
+	count   int
+	root    *node
 }
 
 // Compare returns an integer comparing the two items
@@ -12,11 +12,9 @@ type Treap struct {
 type Compare func(a, b Item) int
 
 // Key and Item can be anything.
-type Key interface{}
 type Item interface{}
 
 type node struct {
-	key      Key
 	item     Item
 	priority int
 	left     *node
@@ -24,20 +22,33 @@ type node struct {
 }
 
 func NewTreap(c Compare) *Treap {
-	return &Treap{compare: c, root: nil}
+	return &Treap{compare: c, count: 0, root: nil}
 }
 
 func (t *Treap) Count() int {
 	return t.count
 }
 
-func (t *Treap) Get(key Key) Item {
+func (t *Treap) Get(target Item) Item {
 	if t.root == nil {
 		return nil
 	}
-	return t.root.get(key)
+	return t.get(t.root, target)
 }
 
-func (n *node) get(key Key) Item {
+func (t *Treap) get(n *node, target Item) Item {
+	for {
+		if n == nil {
+			break
+		}
+		c := t.compare(target, n.item)
+		if c < 0 {
+			n = n.left
+		} else if c > 0 {
+			n = n.right
+		} else {
+			return n.item
+		}
+	}
 	return nil
 }
