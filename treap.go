17c51a6341397b44498c7ff6b152f3ad58ddc36b
Steve Yen <steve.yen@gmail.com>
2012-12-26 19:24:02 -0800

    Define Compare and Item types.

--- treap.go
+++ treap.go
@@ -1,4 +1,14 @@
 package treap
 
 type Treap struct {
+	compare Compare
 }
+
+// Compare returns an integer comparing the two items
+// lexicographically. The result will be 0 if a==b, -1 if a < b, and
+// +1 if a > b.
+type Compare func(a, b Item) int
+
+// Item can be anything.
+type Item interface{}
+
